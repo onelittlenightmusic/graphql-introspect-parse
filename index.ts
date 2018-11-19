7a64e614003815176247ca31de2c8c3299286888
Hiroyuki OSAKI <hiroyuki.osaki@gmail.com>
2018-11-20 00:07:35 +0900

    release

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-import { generateStarSchema } from './schemagen'
-
-export const createStarSchema = async (fileName: string, hint?: any) => {
-    return await generateStarSchema(fileName, hint)
-}
+export * from './inspectSchemaForType'
